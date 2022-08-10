fb743d3776a282be64711d9c1d0a9889fc3d89c6
stabe145 <stabe145@student.otago.ac.nz>
2022-08-10 12:30:33 +1200

    Creating the project and building the menu screen

---
+++ COSC345-Lang/COSC345-Lang/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COSC345-Lang
+//
+//  Created by Ben Stacey on 10/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
