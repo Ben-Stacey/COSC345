14d815055875744c54a6af45435afb04e4ae3e97
stabe145 <stabe145@student.otago.ac.nz>
2022-08-16 16:15:17 +1200

    Implementing Level control file

--- COSC345-Lang/COSC345-Lang/Classes/Models/GameData.swift
+++ COSC345-Lang/COSC345-Lang/Classes/Models/GameData.swift
@@ -1,3 +1,9 @@
+//
+//  GameData.swift
+//  COSC345-Lang
+//
+//  Created by Ben Stacey on 16/08/22.
+//
 import Foundation
 
 class GameData {
---
+++ COSC345-Lang/COSC345-Lang/Classes/Models/Level.swift
@@ -0,0 +1,30 @@
+//
+//  Level.swift
+//  COSC345-Lang
+//
+//  Created by Ben Stacey on 16/08/22.
+//
+
+import Foundation
+struct Level {
+  let pointsPerTile: Int
+  let timeToSolve: Int
+  let anagrams: [NSArray]
+  
+  init(levelNumber: Int) {
+    //1 find .plist file for this level
+    let fileName = "level\(levelNumber).plist"
+    let levelPath = "\(Bundle.main.resourcePath!)/\(fileName)"
+    
+    //2 load .plist file
+    let levelDictionary: NSDictionary? = NSDictionary(contentsOfFile: levelPath)
+    
+    //3 validation
+    assert(levelDictionary != nil, "Level configuration file not found")
+    
+    //4 initialize the object from the dictionary
+    self.pointsPerTile = levelDictionary!["pointsPerTile"] as! Int
+    self.timeToSolve = levelDictionary!["timeToSolve"] as! Int
+    self.anagrams = levelDictionary!["anagrams"] as! [NSArray]
+  }
+}
