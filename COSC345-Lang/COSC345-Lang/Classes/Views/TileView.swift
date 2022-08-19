dc4de730b001366a3980dd5a3c2e6c04e81c37ad
stabe145 <stabe145@student.otago.ac.nz>
2022-08-19 13:40:40 +1200

    Fixing errors in TIleView

--- COSC345-Lang/COSC345-Lang/Classes/Views/TileView.swift
+++ COSC345-Lang/COSC345-Lang/Classes/Views/TileView.swift
@@ -18,7 +18,7 @@ class TileView:UIImageView{
     
     init(letter:Character, sideLength:CGFloat){
         self.letter = letter
-        let image = UIImage(named: "tile")
+        let image = UIImage(named: "tile")!
         super.init(image:image)
         let scale = sideLength / image.size.width
         self.frame = CGRect(x: 0, y: 0, width: image.size.width * scale, height: image.size.height * scale)
