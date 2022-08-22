3be4d08b8810fc658022756c3700fae81cb1bc54
abdma944 <abdma944@student.otago.ac.nz>
2022-08-23 10:15:29 +1200

    added return to sea of word start screen after results

--- COSC345-Lang/COSC345-Lang/SeaofWordsResult.swift
+++ COSC345-Lang/COSC345-Lang/SeaofWordsResult.swift
@@ -4,14 +4,18 @@ import SwiftUI
 //Result Screen for Sea of Words
 
 class SeaofWordsResult: UIViewController {
+
     weak var resultLabel: UILabel!
+
+    var result = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        resultLabel.text = "\(result)"
         // Do any additonal setup after loading
     }
 
     func onClickHome(_ sender: Any) {
-
+        navigationController?.popToRootViewController(animate: true)
     }
 }
