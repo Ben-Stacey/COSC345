0428b07d8b667bca6d4b07ccfea805c7f83aeabc
stabe145 <stabe145@student.otago.ac.nz>
2022-08-24 17:08:03 +1200

    Fixing the button control problem for the start button

--- COSC345-Lang/COSC345-Lang/MainMenu.swift
+++ COSC345-Lang/COSC345-Lang/MainMenu.swift
@@ -11,30 +11,43 @@ class MainMenu: UIViewController {
     
     var language = ""
     var name = ""
+    var checkOne = false
+    var checkTwo = false
+    
     
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var textfield: UITextField!
+    @IBOutlet weak var start: UIButton!
     
     @IBAction func maoriButtion(_ sender: Any) {
         language = "maori"
         label.text = (language)
+        checkTwo = true
     }
     
     @IBAction func frenchButton(_ sender: Any) {
         language = "french"
         label.text = (language)
+        checkTwo = true
     }
     
     @IBAction func spanishButton(_ sender: Any) {
         language = "spanish"
         label.text = (language)
+        checkTwo = true
     }
     
-    @IBAction func startButton(_ sender: Any) {
-        if let n = textfield.text, !(language.isEmpty){
+    @IBAction func start(_ sender: Any) {
+        if let n = textfield.text{
             name = n
+        }
+    }
+    
+    @IBSegueAction func startReady(_ coder: NSCoder) -> HomeScreen? {
+        if !language.isEmpty, !name.isEmpty{
+            return HomeScreen(coder: coder)
         }else{
-            print("error")
+            return nil
         }
     }
     
