3631e3b24246994df0fb071eb5edfcc6d75afc7c
stabe145 <stabe145@student.otago.ac.nz>
2022-08-25 16:45:02 +1200

    Adding wiki comments to the HUDView file

--- COSC345-Lang/COSC345-Lang/Classes/Views/HUDView.swift
+++ COSC345-Lang/COSC345-Lang/Classes/Views/HUDView.swift
@@ -13,10 +13,18 @@ class HUDView: UIView {
   //var gamePoints: CounterLabelView
   //hintButton: UIButton!
     
+    /**
+        Init shouldnt be called and if it is diaplays the fatal error
+    */
     required init(coder aDecorder:NSCoder){
         fatalError("use init(frame:")
     }
     
+    /**
+        Enables the user interaction for the HUD on the game
+        - Parameters:
+            - frame: [in] This is the frame of HUD to be displayed in the game
+    */
     override init(frame:CGRect){
         super.init(frame:frame)
         self.isUserInteractionEnabled = true
