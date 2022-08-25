55c8697f2703a02d040f98699cb7740b0de8ed47
stabe145 <stabe145@student.otago.ac.nz>
2022-08-25 16:27:28 +1200

    Adding wiki comments to the Config file

--- COSC345-Lang/COSC345-Lang/Config.swift
+++ COSC345-Lang/COSC345-Lang/Config.swift
@@ -13,6 +13,14 @@ let ScreenHeight = UIScreen.main.bounds.size.height
 
 let TileMargin: CGFloat = 20.0
 
+/**
+    Randomly generates a number that between the two limits
+    - Parameters:
+        - minX: [in] lower limit of the random number
+        - maxX: [in] upper limit of the random number
+    - Returns:
+        - Int(result): returns the randomly generated number
+*/
 func randomNumber(minX:UInt32, maxX:UInt32) -> Int {
   let result = (arc4random() % (maxX - minX + 1)) + minX
   return Int(result)
