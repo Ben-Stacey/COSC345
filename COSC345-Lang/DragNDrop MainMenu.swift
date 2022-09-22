7064e99f0bd619b0255c02a24b5798f0e23262b2
Ben <benjaminstacey87@gmail.com>
2022-09-22 15:44:25 +1200

    Adding view controller for the drag n drop menu

---
+++ COSC345-Lang/DragNDrop MainMenu.swift
@@ -0,0 +1,29 @@
+//
+//  DragNDrop MainMenu.swift
+//  COSC345-Lang
+//
+//  Created by Ben Stacey on 22/09/22.
+//
+
+import UIKit
+
+class DragNDrop_MainMenu: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
