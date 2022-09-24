a246ac28e8f14cd27eb9ca2ab598be8fef172495
Ben <benjaminstacey87@gmail.com>
2022-09-24 15:02:21 +1200

    Fixing colons

--- COSC345-Lang/COSC345-Lang/QuizMenuViewController.swift
+++ COSC345-Lang/COSC345-Lang/QuizMenuViewController.swift
@@ -10,13 +10,13 @@ import UIKit
 /**
     The view controller for the quiz menu.
 */
-public class QuizMenuViewController: UIViewController {
-    @IBOutlet weak var playButton: UIButton! {
+public class QuizMenuViewController:UIViewController {
+    @IBOutlet weak var playButton:UIButton! {
         didSet{
             playButton.layer.cornerRadius = playButton.frame.height/2
         }
     }
-    @IBOutlet weak var backButton: UIButton! {
+    @IBOutlet weak var backButton:UIButton! {
         didSet{
             backButton.layer.cornerRadius = backButton.frame.height/2
         }
@@ -36,7 +36,7 @@ public class QuizMenuViewController: UIViewController {
         - Parameters:
             - sender: allows method to be sent anything
     */
-    @IBAction public func onClickPlay(_ sender: Any) {
+    @IBAction public func onClickPlay(_ sender:Any) {
         guard let vc = storyboard?.instantiateViewController(withIdentifier: "QuizViewController") as? QuizViewController else{return}
         self.navigationController?.pushViewController(vc, animated: true)
     }
